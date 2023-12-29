b9bc93ba60aa1eb5857079d8a66aaf46aa9bd70b
Olga Cazacioc <1697327@cstjean.qc.ca>
2023-12-29 00:23:48 -0500

    insert data

--- app.js
+++ app.js
@@ -10,19 +10,23 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
     // content type and status code set automatically
     // res.send('<p>Home<p>');
     // res.sendFile('./views/index.html', {root: __dirname});
-    res.render('index');
+    res.render('index', {title: 'Home', blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', {title: 'About'});
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', {title: 'Create'});
 });
 
 // redirect
@@ -32,5 +36,5 @@ app.get('/blogs/create', (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {title: '404'});
 });
