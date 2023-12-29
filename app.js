573c3dadd239dc837da212f3c9f6dfa941930a22
Olga Cazacioc <1697327@cstjean.qc.ca>
2023-12-29 00:06:03 -0500

    install EJS, add create page

--- app.js
+++ app.js
@@ -2,11 +2,35 @@ const express = require('express');
 
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
     
     // content type and status code set automatically
-    res.send('<p>Home<p>');
+    // res.send('<p>Home<p>');
+    // res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+});
+
+// redirect
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+// });
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404');
 });
