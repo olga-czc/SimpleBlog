99ae12c1909d1dd47062239d9811fd1aec9f0a00
Olga Cazacioc <1697327@cstjean.qc.ca>
2024-01-08 13:53:21 -0500

    add MongoDB

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
 
-// register view engine
+//connect to mongodb
+const dbURI = 'mongodb+srv://test:qwert12345@nodeblog.nn3bhvv.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
+    .then((result) => console.log('connected to db'))
+    .catch((err) => console.log(err));
+
+    // register view engine
 app.set('view engine', 'ejs');
 
 
 // listen for requests
-app.listen(3000);
+// app.listen(3000);
 
 // middleware
 // app.use((req, res, next) => {
